6dc1fa48c0acfc97de91152822c59ed83edee2f7
Peter Bronkalla <peterbronkalla@googlemail.com>
2014-11-09 21:51:57 +0100

    Modified vagrant provision to install MySQL

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-apt-get update
+apt-get update > /dev/null
 
 # apache configuration
-apt-get install -y apache2 php5 libapache2-mod-php5
+apt-get install -y apache2 php5 libapache2-mod-php5 > /dev/null
 rm -rf /var/www/html
 ln -sf /vagrant /var/www/html
 
+# PHP5
+echo "Installing PHP"
+apt-get install -y php5 libapache2-mod-php5 php5-common php5-dev php5-cli php5-fpm > /dev/null
+apt-get install -y curl php5-curl php5-gd php5-mcrypt > /dev/null
+
+
 # mysql
-# TODO make initial mysql setup
-# apt-get install -y mysql-server mysql-client php5-mysql
+echo "Installing and configurating MySQL"
+apt-get install -y debconf-utils > /dev/null
+debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+apt-get install -y mysql-server > /dev/null
+apt-get install -y mysql-client php5-mysql > /dev/null
